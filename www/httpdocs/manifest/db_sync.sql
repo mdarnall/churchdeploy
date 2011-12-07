94cb0255649b6df19db6e1c2efbd521b88cc2b49
Jonathan Simcoe <jsimcoe@webmd.net>
2011-12-07 12:46:30 -0800

    Footer changes

--- www/httpdocs/manifest/db_sync.sql
+++ www/httpdocs/manifest/db_sync.sql
@@ -26,3 +26,9 @@ DELETE FROM `sym_fields_stage_sorting` WHERE `entry_id` = '103';
 INSERT INTO `sym_fields_stage_sorting` (`entry_id`, `field_id`, `order`) VALUES ('103', '64', '107');
 INSERT INTO `sym_fields_stage_sorting` (`entry_id`, `field_id`, `order`) VALUES ('103', '65', '0');
 INSERT INTO `sym_fields_stage_sorting` (`entry_id`, `field_id`, `order`) VALUES ('103', '66', '107');
+
+-- 2011-12-07 11:33:52, Jonathan Simcoe, http://churchdeploy/symphony/publish/tags/edit/43/
+DELETE FROM `sym_fields_stage_sorting` WHERE `entry_id` = '43';
+INSERT INTO `sym_fields_stage_sorting` (`entry_id`, `field_id`, `order`) VALUES ('43', '64', '112,108');
+INSERT INTO `sym_fields_stage_sorting` (`entry_id`, `field_id`, `order`) VALUES ('43', '65', '0');
+INSERT INTO `sym_fields_stage_sorting` (`entry_id`, `field_id`, `order`) VALUES ('43', '66', '0');
