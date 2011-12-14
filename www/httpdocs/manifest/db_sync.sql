eae57d61075ab606b96baa29e2bd645df1c86f68
Jonathan Simcoe <jsimcoe@webmd.net>
2011-12-14 13:57:17 -0800

    don't know what changed

---
+++ www/httpdocs/manifest/db_sync.sql
@@ -0,0 +1,6 @@
+
+-- 2011-12-07 16:04:27, Jonathan Simcoe, http://churchdeploy/symphony/publish/tags/edit/43/
+DELETE FROM `sym_fields_stage_sorting` WHERE `entry_id` = '43';
+INSERT INTO `sym_fields_stage_sorting` (`entry_id`, `field_id`, `order`) VALUES ('43', '64', '112,108');
+INSERT INTO `sym_fields_stage_sorting` (`entry_id`, `field_id`, `order`) VALUES ('43', '65', '0');
+INSERT INTO `sym_fields_stage_sorting` (`entry_id`, `field_id`, `order`) VALUES ('43', '66', '0');
