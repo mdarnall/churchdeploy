cc919ff7f5ba4093366cc8c7e08d36948329507b
kirkstrobeck <kirk@strobeck.com>
2012-05-28 17:52:17 -0700

    updates

---
+++ www/httpdocs/workspace/data-sources/data.teachings_series_all_entries_filtered.php
@@ -0,0 +1,77 @@
+<?php
+
+	require_once(TOOLKIT . '/class.datasource.php');
+
+	Class datasourceteachings_series_all_entries_filtered extends Datasource{
+
+		public $dsParamROOTELEMENT = 'teachings-series-all-entries-filtered';
+		public $dsParamORDER = 'desc';
+		public $dsParamPAGINATERESULTS = 'yes';
+		public $dsParamLIMIT = '12';
+		public $dsParamSTARTPAGE = '1';
+		public $dsParamREDIRECTONEMPTY = 'no';
+		public $dsParamSORT = 'system:id';
+		public $dsParamHTMLENCODE = 'yes';
+		public $dsParamASSOCIATEDENTRYCOUNTS = 'yes';
+
+		public $dsParamFILTERS = array(
+				'232' => '(if value of ({$pt2}) is (teachings))',
+				'233' => 'no',
+		);
+
+		public $dsParamINCLUDEDELEMENTS = array(
+				'name',
+				'poster',
+				'teachings: title'
+		);
+
+
+		public function __construct(&$parent, $env=NULL, $process_params=true){
+			parent::__construct($parent, $env, $process_params);
+			$this->_dependencies = array();
+		}
+
+		public function about(){
+			return array(
+				'name' => 'Teachings: Series: All entries (filtered)',
+				'author' => array(
+					'name' => 'Kirk Strobeck',
+					'website' => 'http://72.10.33.203',
+					'email' => 'kirk@strobeck.com'),
+				'version' => 'Symphony 2.2.5',
+				'release-date' => '2012-05-28T20:11:37+00:00'
+			);
+		}
+
+		public function getSource(){
+			return '27';
+		}
+
+		public function allowEditorToParse(){
+			return true;
+		}
+
+		public function grab(&$param_pool=NULL){
+			$result = new XMLElement($this->dsParamROOTELEMENT);
+
+			try{
+				include(TOOLKIT . '/data-sources/datasource.section.php');
+			}
+			catch(FrontendPageNotFoundException $e){
+				// Work around. This ensures the 404 page is displayed and
+				// is not picked up by the default catch() statement below
+				FrontendPageNotFoundExceptionHandler::render($e);
+			}
+			catch(Exception $e){
+				$result->appendChild(new XMLElement('error', $e->getMessage()));
+				return $result;
+			}
+
+			if($this->_force_empty_result) $result = $this->emptyXMLSet();
+
+			
+
+			return $result;
+		}
+
+	}
