e573687e3390fe8d3fae7ae5ae5035e36d4bbfef
Matt Darnall <mdarnall@gmail.com>
2012-09-01 14:13:09 -0700

    remove debug statements

--- www/httpdocs/workspace/themes/active/js/alertManager.js
+++ www/httpdocs/workspace/themes/active/js/alertManager.js
@@ -10,16 +10,14 @@
       return $.cookie(this.alertCookieName) || [] ;
     },
     saveHiddenAlerts : function(alerts) {
-      $.cookie(this.alertCookieName, alerts, { json: true, expires: 365, path : "/" });
+      $.cookie(this.alertCookieName, alerts, { expires: 365, path : "/" });
     },
 
     shouldAlertShow : function (id){
-      window.console.log('shouldAlertShow');
       var alerts = this.getHiddenAlerts();
       return (alerts.indexOf(id) < 0);
     }, 
     markAlertHidden : function (id) {
-      window.console.log('markAlertHidden ' + id);
       var alerts = this.getHiddenAlerts();
       alerts.push(id);
       this.saveHiddenAlerts(alerts);
